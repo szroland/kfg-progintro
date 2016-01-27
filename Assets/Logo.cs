b2021c2951c05fc73e8e7ec2314c100c1ba5aa8b
szroland <szroland@freemail.hu>
2016-01-27 19:35:28 +0100

    Fordul / Elore pelda vezérlő

---
+++ Assets/Logo.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Logo : ScriptedController
+{
+
+    public GameObject kidobottObjektum;
+
+    override public void Run()
+    {
+        Varakozik(2);
+        Felirat("Felszáll");
+        Motor(true);
+        
+        for (;;)
+        {
+            Fordul(60);
+            Elore(10);
+            Kidob(kidobottObjektum);
+        }
+      
+    }
+
+}
