8b79eadc66fe4b07a043d743cafeaf8a7248f958
szroland <szroland@freemail.hu>
2016-02-09 22:36:58 +0100

    Alap bomba, robbanás effekt nélkül

---
+++ Assets/Scripts/Bomba.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomba : MonoBehaviour {
+
+    public float hatosugar = 10;
+    public float robbanoEro = 5;
+    public float elesSebesseg = 4;
+
+    private Rigidbody rb;
+    private Light elesFeny;
+    private bool eles = false;
+
+	void Start () {
+        elesFeny = GetComponentInChildren<Light>();
+        rb = GetComponent<Rigidbody>();
+        elesFeny.enabled = false;	
+	}
+	
+    void FixedUpdate()
+    {
+        if (rb.velocity.magnitude > elesSebesseg)
+        {
+            eles = true;
+            elesFeny.enabled = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!eles)
+            return;
+
+        ContactPoint cp = collision.contacts[0];
+
+        Collider[] colliders = Physics.OverlapSphere(cp.point, hatosugar);
+        foreach (Collider c in colliders) {
+            Rigidbody rb = c.attachedRigidbody; 
+            if (rb)
+            {
+                rb.AddExplosionForce(robbanoEro, cp.point, hatosugar, 0.5f, ForceMode.Impulse);
+            }
+        }
+        Destroy(gameObject);
+    }
+
+}
